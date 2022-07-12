e84e84b4ed71b8ed11177731403ba6a7555e825b
Manh Tuong <mtuong669@gmail.com>
2022-07-12 08:23:31 +0700

    Cấu hình Router/Layout

---
+++ src/Routes/index.js
@@ -0,0 +1,15 @@
+import Home from '~/Pages/Home';
+import Following from '~/Pages/Following';
+import Upload from '~/Pages/Upload';
+import { HeaderOnly } from '~/components/Layout';
+
+//Public Routes
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
